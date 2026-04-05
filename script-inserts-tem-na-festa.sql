5cf8a3732b033b23bab980a28d73c34601577ca8
pedrorico79 <pedro.rico@sptech.school>
2026-04-04 22:16:24 -0300

    finalizando criacao das tabelas | ajustando inserts tests

--- script-inserts-tem-na-festa.sql
+++ script-inserts-tem-na-festa.sql
@@ -1,4 +1,5 @@
 USE tem_na_festa;
+
 -- ENDERECO
 INSERT INTO endereco (cep, logradouro, numero, complemento, bairro, cidade, estado)
 VALUES ('12345678', 'Rua das Flores', '123', 'Apto 101', 'Centro', 'São Paulo', 'SP');
@@ -9,7 +10,9 @@ VALUES ('Maria Silva', '11999999999', '11988888888', '@mariafestas', '2026-04-01
 
 -- PERFIL
 INSERT INTO perfil (nome)
-VALUES ('Administrador');
+VALUES ('Administrador'),
+('Atendente'),
+('Producao');
 
 -- USUARIO
 INSERT INTO usuario (nome, email, senha, is_ativo, data_criacao, perfil_id)
@@ -17,19 +20,52 @@ VALUES ('João Admin', 'admin@festa.com', '123456', TRUE, NOW(), 1);
 
 -- STATUS PRODUCAO
 INSERT INTO status_producao (nome)
-VALUES ('Em produção');
+VALUES ('NAO_INICIADO'),
+('EM_PREPARO'),
+('FINALIZADO'),
+('ENTREGUE');
 
 -- PRODUTO
 INSERT INTO produto (nome, descricao, preco_venda, is_ativo)
 VALUES ('Bolo de Chocolate', 'Bolo grande para festas', 150.00, TRUE);
 
--- PEDIDO
+-- ------------------------------------------------------------
+-- PEDIDO (começa como NAO_INICIADO = id 1)
 INSERT INTO pedido (data_pedido, data_entrega, valor_total, observacao, cliente_id, status_producao_id, usuario_id)
 VALUES (NOW(), '2026-04-10 15:00:00', 150.00, 'Entrega no período da tarde', 1, 1, 1);
 
+-- HISTORICO INICIAL
+INSERT INTO historico_status_pedido 
+(data_alteracao, observacao, status_producao_id, pedido_id, usuario_id)
+VALUES 
+(NOW(), 'Pedido criado', 1, 1, 1);
+
+-- SIMULANDO MUDANÇA DE STATUS → EM_PREPARO
+UPDATE pedido SET status_producao_id = 2 WHERE id = 1;
+
+INSERT INTO historico_status_pedido 
+(data_alteracao, observacao, status_producao_id, pedido_id, usuario_id)
+VALUES (NOW(), 'Pedido em preparo', 2, 1, 1);
+
+-- FINALIZADO
+UPDATE pedido SET status_producao_id = 3 WHERE id = 1;
+
+INSERT INTO historico_status_pedido 
+(data_alteracao, observacao, status_producao_id, pedido_id, usuario_id)
+VALUES (NOW(), 'Pedido finalizado', 3, 1, 1);
+
+-- ENTREGUE
+UPDATE pedido SET status_producao_id = 4 WHERE id = 1;
+
+INSERT INTO historico_status_pedido 
+(data_alteracao, observacao, status_producao_id, pedido_id, usuario_id)
+VALUES (NOW(), 'Pedido entregue', 4, 1, 1);
+-- ------------------------------------------------------------
+
 -- PAGAMENTO
 INSERT INTO pagamento (valor, data_pagamento, metodo, pedido_id, usuario_id)
-VALUES (150.00, NOW(), 'Pix', 1, 1);
+VALUES (100.00, NOW(), 'PIX', 1, 1),
+(50.00, NOW(), 'DEBITO', 1, 1);
 
 -- PEDIDO_PRODUTO
 INSERT INTO pedido_produto (pedido_id, produto_id, quantidade, preco_unitario)
